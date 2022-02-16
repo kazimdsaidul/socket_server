bb654d0abba98572a915ca4b372df7fc2584b9d2
kazi-sit <84772892+kazi-sit@users.noreply.github.com>
2022-02-16 15:55:59 +0600

    initilization: setup server and client both

---
+++ index.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const app = express();
+const http = require('http');
+const expressServer = http.createServer(app);
+
+
+const { Server } = require('socket.io');
+
+const io = new Server(expressServer);
+
+// handle all event all to server
+io.on('connection', function(socket) {
+    console.log("New User Connected!")
+
+    setTimeout(function() {
+        console.log("send message to client")
+        socket.send("I am message from server.....")
+    }, 5000)
+
+    socket.on("disconnect", function() {
+        console.log("User disconnected")
+    })
+
+})
+
+
+//server redrirect to index.html file 
+app.get("/", function(req, res) {
+    res.sendFile(__dirname + "/index.html")
+});
+
+expressServer.listen(3000, function() {
+    console.log("server run @3000");
+})
